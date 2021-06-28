c36689778bf15a2f750aa7c9244fc0a716ba396c
chriscyhxx <littlesun95018@gmail.com.tw>
2021-06-29 01:43:38 +0800

    test

--- src/Gameplay.java
+++ src/Gameplay.java
@@ -8,11 +8,21 @@ import java.awt.*;
 import javax.swing.*;
 import javax.swing.Timer;
 public class Gameplay extends JPanel implements ActionListener {
+    private Player player1;
+    private Player player2;
     private Brick solid_brick;
     private Brick breakable_brick;
     public Gameplay(){
         solid_brick = new Solid_Bricks();
         breakable_brick = new Breakable_Bricks();
+        player1 = new Player(200, 500, false, false, false, true, "player1_tank_right",
+                "player1_tank_left", "player1_tank_up", "player1_tank_down");
+        player2 = new Player(400, 500, false, false, false, true, "player2_tank_right",
+                "player2_tank_left", "player2_tank_up", "player2_tank_down");
+        player1.BrickList.add(solid_brick);
+        player1.BrickList.add(breakable_brick);
+        player2.BrickList.add(solid_brick);
+        player2.BrickList.add(breakable_brick);
     }
     public void paint(){
 
@@ -21,8 +31,23 @@ public class Gameplay extends JPanel implements ActionListener {
     public void actionPerformed(ActionEvent e){
         repaint();
     }
-    public void setBrick(){
+    private void setBrick(){
         solid_brick = new Solid_Bricks();
         breakable_brick = new Breakable_Bricks();
+    }
+
+    private class resetListener implements KeyListener{
+        public void keyTyped(KeyEvent e) {}
+        public void keyReleased(KeyEvent e) {}
+        public void keyPressed(KeyEvent e){
+            if(e.getKeyCode() == KeyEvent.VK_SPACE && (player1.lives == 0 || player2.lives == 0)){
+                setBrick();
+                player1 = new Player(200, 500, false, false, false, true, "player1_tank_right",
+                        "player1_tank_left", "player1_tank_up", "player1_tank_down");
+                player2 = new Player(400, 500, false, false, false, true, "player2_tank_right",
+                        "player2_tank_left", "player2_tank_up", "player2_tank_down");
+            }
+
+        }
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,26 +1,27 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
 
 public class Player {
     private ArrayList<Weapon> WeaponList;
-    private ImageIcon player;
-    private
-    private int X;
-    private int Y;
-    private boolean right;
-    private boolean left;
-    private boolean down;
-    private boolean up;
-    private int score = 0;
-    private int lives = 5;
-    private boolean Shoot;
-    private String shootDirection = "";
+    private ImageIcon playerIMG;
+    public int X;
+    public int Y;
+    public boolean right;
+    public boolean left;
+    public boolean down;
+    public boolean up;
+    public int score = 0;
+    public int lives = 5;
+    public boolean Shoot;
+    public String shootDirection = "";
     private String right_img;
     private String left_img;
     private String up_img;
     private String down_img;
+    public ArrayList<Brick> BrickList;
     public Player(int x, int y, boolean r, boolean l, boolean d, boolean u, String RightIMG, String LeftIMG, String UpIMG, String DownIMG){
         X = x;
         Y = y;
@@ -32,6 +33,13 @@ public class Player {
         left_img = LeftIMG;
         up_img = UpIMG;
         down_img = DownIMG;
+        if(u)
+            playerIMG = new ImageIcon(UpIMG);
+        else if(d)
+            playerIMG = new ImageIcon(DownIMG);
+        else if(r)
+            playerIMG = new ImageIcon(RightIMG);
+        else playerIMG = new ImageIcon(LeftIMG);
         Bullet bullet = new Bullet();
         Missile missile = new Missile();
         Bomb bomb = new Bomb(); //我不確定constructer長怎樣所以先寫這樣
@@ -39,114 +47,93 @@ public class Player {
         WeaponList.add(missile);
         WeaponList.add(bomb);
     }
-
-
-
+    public ImageIcon getPlayerIMG(){
+        return playerIMG;
+    }
     private class Listener implements KeyListener{
         public void keyTyped(KeyEvent e) {}
         public void keyReleased(KeyEvent e) {}
         public void keyPressed(KeyEvent e) {
-            if(e.getKeyCode() == KeyEvent.VK_SPACE && (player1lives == 0 || player2lives == 0))
-            {
-                br = new brick();
-                X = 200;
-                Y = 550;
+            if(e.getKeyCode()== KeyEvent.VK_W){
                 right = false;
                 left = false;
-                player1down = false;
-                player1up = true;
-
-                player2X = 400;
-                player2Y = 550;
-                player2right = false;
-                player2left = false;
-                player2down = false;
-                player2up = true;
-
-                player1score = 0;
-                player1lives = 5;
-                player2score = 0;
-                player2lives = 5;
-                play = true;
-                repaint();
-            }
-            if(e.getKeyCode()== KeyEvent.VK_U)
-            {
-                if(!player1Shoot)
-                {
-                    if(player1up)
-                    {
-                        player1Bullet = new Player1Bullet(player1X + 20, player1Y);
+                down = false;
+                up = true;
+                boolean testCollision = false;
+                if(Y >= 10){
+                    for(int i = 0; i < BrickList.size(); ++i){
+                        Brick b = BrickList.get(i);
+                        if(testCollision) break;
+                        for(int j = 0; j < b.brickOn.length; ++j){
+                            if (new Rectangle(X, Y - 10, 10, 10).intersects(new Rectangle(b.brickXpos[j], b.brickYpos[j], b.brickWidth, b.brickHeight))) {
+                                testCollision = true;
+                                break;
+                            }
+                        }
                     }
-                    else if(player1down){
-                        player1Bullet = new Player1Bullet(player1X + 20, player1Y + 40);
-                    }
-                    else if(player1right){
-                        player1Bullet = new Player1Bullet(player1X + 40, player1Y + 20);
-                    }
-                    else if(player1left)
-                    {
-                        player1Bullet = new Player1Bullet(player1X, player1Y + 20);
-                    }
-
-                    player1Shoot = true;
+                    if(!testCollision) Y -= 10;
                 }
             }
-            if(e.getKeyCode()== KeyEvent.VK_W)
-            {
-                player1right = false;
-                player1left = false;
-                player1down = false;
-                player1up = true;
-
-                if(!(player1Y < 10)){
-                    player1Y-=10;
-                }
-
-
-            }
-            if(e.getKeyCode()== KeyEvent.VK_A)
-            {
-                player1right = false;
-                player1left = true;
-                player1down = false;
-                player1up = false;
-
-                if(!(player1X < 10)){
-                    player1X-=10;
-                }
-
-            }
-            if(e.getKeyCode()== KeyEvent.VK_S)
-            {
-                player1right = false;
-                player1left = false;
-                player1down = true;
-                player1up = false;
-
-                if(!(player1Y > 540)){
-                    boolean check = false;
-                    for(int i = 0; i < br.bricksYPos.length ; ++i){
-                        if(player1Y + 10>= br.bricksYPos[i] && player1X == br.bricksXPos[i])
-                            check = true;
+            if(e.getKeyCode()== KeyEvent.VK_A){
+                right = false;
+                left = true;
+                down = false;
+                up = false;
+                boolean testCollision = false;
+                if(X >= 10){
+                    for(int i = 0; i < BrickList.size(); ++i){
+                        Brick b = BrickList.get(i);
+                        if(testCollision) break;
+                        for(int j = 0; j < b.brickOn.length; ++j){
+                            if (new Rectangle(X - 10, Y, 10, 10).intersects(new Rectangle(b.brickXpos[j], b.brickYpos[j], b.brickWidth, b.brickHeight))) {
+                                testCollision = true;
+                                break;
+                            }
+                        }
                     }
-                    if(!check) player1Y+=10;
+                    if(!testCollision) X -= 10;
                 }
-
             }
-            if(e.getKeyCode()== KeyEvent.VK_D)
-            {
-                player1right = true;
-                player1left = false;
-                player1down = false;
-                player1up = false;
-
-                if(!(player1X > 590)){
-                    player1X+=10;
+            if(e.getKeyCode()== KeyEvent.VK_S){
+                right = false;
+                left = false;
+                down = true;
+                up = false;
+                boolean testCollision = false;
+                if(Y <= 540){
+                    for(int i = 0; i < BrickList.size(); ++i){
+                        Brick b = BrickList.get(i);
+                        if(testCollision) break;
+                        for(int j = 0; j < b.brickOn.length; ++j){
+                            if (new Rectangle(X, Y + 10, 10, 10).intersects(new Rectangle(b.brickXpos[j], b.brickYpos[j], b.brickWidth, b.brickHeight))) {
+                                testCollision = true;
+                                break;
+                            }
+                        }
+                    }
+                    if(!testCollision) Y += 10;
                 }
-
+            }
+            if(e.getKeyCode()== KeyEvent.VK_D){
+                right = true;
+                left = false;
+                down = false;
+                up = true;
+                boolean testCollision = false;
+                if(X <= 540){
+                    for(int i = 0; i < BrickList.size(); ++i){
+                        Brick b = BrickList.get(i);
+                        if(testCollision) break;
+                        for(int j = 0; j < b.brickOn.length; ++j){
+                            if(new Rectangle(X + 10, Y, 10, 10).intersects(new Rectangle(b.brickXpos[j], b.brickYpos[j], b.brickWidth, b.brickHeight))) {
+                                testCollision = true;
+                                break;
+                            }
+                        }
+                    }
+                    if(!testCollision) X += 10;
+                }
             }
         }
-    }
     }
 }

d8a3fbabcad1ef1794b6874c3d74f3e933138a30
Warron Yiang <warronyiang@Warrons-MacBook-Pro.local>
2021-06-29 02:42:22 +0800

    update Weapon.java create function

--- src/Weapon.java
+++ src/Weapon.java
@@ -2,12 +2,15 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Rectangle;
 
 import javax.swing.ImageIcon;
 
 public abstract class Weapon {
     public abstract void perform(int n,Player player,Breakable_Bricks bricks);
-    public abstract void draw(Graphics g);
+    public abstract void draw(Component c,Graphics g);
     public abstract int getX();
     public abstract int getY();
     public abstract void move(String face);

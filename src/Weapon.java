d124c15666a072c02d5d98f5c5018b73c6469b8b
Warron Yiang <warronyiang@Warrons-MacBook-Pro.local>
2021-06-29 02:18:00 +0800

    update abstract weapon and add mine.png

--- src/Weapon.java
+++ src/Weapon.java
@@ -1,2 +1,14 @@
-public class Weapon {
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
+import javax.swing.ImageIcon;
+
+public abstract class Weapon {
+    public abstract void perform(int n,Player player,Breakable_Bricks bricks);
+    public abstract void draw(Graphics g);
+    public abstract int getX();
+    public abstract int getY();
+    
 }

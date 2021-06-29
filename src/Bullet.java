13463e62fba8a449361012980a57203920edf347
y10ab1 <b07902144@csie.ntu.edu.tw>
2021-06-29 14:23:18 +0800

    Bullet

--- src/Bullet.java
+++ src/Bullet.java
@@ -1,2 +1,80 @@
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.util.ArrayList;
+
 public class Bullet extends Weapon{
+	private int x, y;
+	public ArrayList<Brick> BrickList;
+	public Bullet(ArrayList<Brick> bList) {
+		BrickList = bList;
+	}
+	
+	@Override
+	public boolean perform(int n, Player user, Player enemy) {
+		if(new Rectangle(getX(), getY(), 10, 10)
+				.intersects(new Rectangle(enemy.X, enemy.Y, 50, 50)))
+				{
+					user.score += 10;
+					enemy.lives -= 1;
+					user.Shoot = false;
+					user.shootDirection = "";
+					return true;
+				}
+		for(Brick br:BrickList)
+			if(br.collisionCheck(1, getX(), getY(), 10, 10))
+			{
+				user.Shoot = false;
+				user.shootDirection = "";	
+				return true;
+			}
+		if(getY() < 1 
+				|| getY() > 580
+				|| getX() < 1
+				|| getX() > 630)
+		{
+			user.Shoot = false;
+			user.shootDirection = "";
+			return true;
+		}
+		return false;
+	}
+	
+	
+
+	@Override
+	public int getX() {
+		return x;
+	}
+
+	@Override
+	public int getY() {
+		return y;
+	}
+
+	@Override
+	public void move(String face) {
+		if(face.equals("right"))
+			x += 5;
+		else if(face.equals("left"))
+			x -= 5;
+		else if(face.equals("up"))
+			y -= 5;
+		else
+			y +=5;
+	}
+
+	@Override
+	public void draw(Component c, Graphics g) {
+		g.setColor(Color.yellow);
+		g.fillOval((int) x, (int) y, 10, 10);
+	}
+
+	@Override
+	public Weapon create() {
+		return new Bullet(BrickList);
+	}
+
+	
 }

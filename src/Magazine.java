759caa4592d95ae98ef862a09677fa55427af3b9
y10ab1 <b07902144@csie.ntu.edu.tw>
2021-06-30 03:10:49 +0800

    mag image

--- src/Magazine.java
+++ src/Magazine.java
@@ -1,17 +1,30 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Arrays;
 
-public class  extends Buff{
-    public ImageIcon N2OImage;
-    public N2O() {
+public class Magazine extends Buff{
+    public ImageIcon MagazineImage;
+    public Magazine() {
         super();
-        N2OImage = new ImageIcon("N2O.png");
+        MagazineImage = new ImageIcon("magazine.png");
+        int x[] = {0};
+        int y[] = {0};
+        buffXpos = x;
+        buffYpos = y;
+        count = new int[1];
+        buffOn = new boolean[1];
+        Arrays.fill(count, 2);
+        Arrays.fill(buffOn, true);
     }
     public void draw(Component component, Graphics graphics){
-
+        for(int i = 0; i < buffOn.length; ++i) {
+            if(buffOn[i])
+                MagazineImage.paintIcon(component, graphics, buffXpos[i], buffYpos[i]);
+        }
     }
-    @Override
-    public boolean acquireCheck(int x, int y, int width, int height){
-        return true;
+
+    public boolean acquireCheck(Player p){
+
+        return false;
     }
 }

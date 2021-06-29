34e770d9197777ed8a1dbc513f35fae1c03d199f
microuzi9797 <ken97971@yahoo.com.tw>
2021-06-30 02:22:44 +0800

    draw health

--- src/Buff.java
+++ src/Buff.java
@@ -1,6 +1,12 @@
+import javax.swing.*;
+
 public class Buff {
-    public int x;
-    public int y;
+    public int buffXpos[];
+    public int buffYpos[];
+    public boolean buffOn[];
+    public int buffWidth;
+    public int buffHeight;
+    public int count[];
     public boolean acquireCheck(int x, int y, int width, int height){
         return true;
     }
--- src/Health.java
+++ src/Health.java
@@ -1,14 +1,26 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Arrays;
 
 public class Health extends Buff{
     public ImageIcon HealthImage;
     public Health() {
         super();
         HealthImage = new ImageIcon("health.png");
+        int x[] = {0};
+        buffXpos = x;
+        int y[] = {0};
+        buffYpos = y;
+        count = new int[1];
+        buffOn = new boolean[1];
+        Arrays.fill(count, 2);
+        Arrays.fill(buffOn, true);
     }
     public void draw(Component component, Graphics graphics){
-
+        for(int i = 0; i < buffOn.length; ++i) {
+            if(buffOn[i])
+                HealthImage.paintIcon(component, graphics, buffXpos[i], buffYpos[i]);
+        }
     }
     @Override
     public boolean acquireCheck(int x, int y, int width, int height){

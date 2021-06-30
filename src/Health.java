0bb87b184b11dc44954ff4b6de0d90ffcce9ac32
microuzi9797 <ken97971@yahoo.com.tw>
2021-06-30 14:11:31 +0800

    fix health

--- src/Health.java
+++ src/Health.java
@@ -4,26 +4,25 @@ import java.util.Arrays;
 
 public class Health extends Buff{
     public ImageIcon HealthImage;
-    public Health() {
+    public Health(int x, int y) {
         super();
         HealthImage = new ImageIcon("health.png");
-        int x[] = {0};
-        buffXpos = x;
-        int y[] = {0};
-        buffYpos = y;
-        count = new int[1];
-        buffOn = new boolean[1];
-        Arrays.fill(count, 2);
-        Arrays.fill(buffOn, true);
+        super.buffXpos = x;
+        super.buffYpos = y;
+        buffOn = true;
     }
     public void draw(Component component, Graphics graphics){
-        for(int i = 0; i < buffOn.length; ++i) {
-            if(buffOn[i])
-                HealthImage.paintIcon(component, graphics, buffXpos[i], buffYpos[i]);
-        }
+        if(super.buffOn)
+            HealthImage.paintIcon(component, graphics, super.buffXpos, super.buffYpos);
     }
     @Override
     public boolean acquireCheck(Player p){
-        return true;
+        if(new Rectangle(super.buffXpos, super.buffYpos, 50, 50)
+                .intersects(new Rectangle(p.X, p.Y, 50, 50))) {
+            p.lives += 1;
+            buffOn = false;
+            return true;
+        }
+        return false;
     }
 }

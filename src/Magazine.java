ab442b9cc90f4865b23b6b252e7b16f33c676bf1
y10ab1 <b07902144@csie.ntu.edu.tw>
2021-06-30 14:22:44 +0800

    remove super

--- src/Magazine.java
+++ src/Magazine.java
@@ -6,19 +6,19 @@ public class Magazine extends Buff{
     public ImageIcon MagazineImage;
     public Magazine(int x, int y) {
         MagazineImage = new ImageIcon("magazine.png");
-        super.buffXpos = x;
-        super.buffYpos = y;
+        buffXpos = x;
+        buffYpos = y;
         buffOn =true;
     }
     public void draw(Component component, Graphics graphics){
 
-        if(super.buffOn)
-            MagazineImage.paintIcon(component, graphics, super.buffXpos, super.buffYpos);
+        if(buffOn)
+            MagazineImage.paintIcon(component, graphics, buffXpos, buffYpos);
 
     }
 
     public boolean acquireCheck(Player p){
-        if(new Rectangle(super.buffXpos, super.buffYpos, 50, 50)
+        if(new Rectangle(buffXpos, buffYpos, 50, 50)
                 .intersects(new Rectangle(p.X, p.Y, 50, 50))) {
             p.getWeaponList().get(1).count+=10;
             p.getWeaponList().get(2).count+=10;
